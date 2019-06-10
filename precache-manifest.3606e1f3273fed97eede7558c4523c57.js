072a3118d57e8bb7c04c4564791a133a683fb0b4
AnnaOrysiak <walachan88@gmail.com>
2019-06-10 21:33:21 +0200

    Updates

---
+++ precache-manifest.3606e1f3273fed97eede7558c4523c57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1cb84a9b73391a94826f98d7f92f7f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cac2a69729d0432a72ca",
+    "url": "/portfolio/static/css/main.44c1cd1f.chunk.css"
+  },
+  {
+    "revision": "00727b378a2960ba7e7f",
+    "url": "/portfolio/static/js/2.1ab2e316.chunk.js"
+  },
+  {
+    "revision": "cac2a69729d0432a72ca",
+    "url": "/portfolio/static/js/main.5cb456c8.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "159fb93a953acb3c18aab7d7c6bab342",
+    "url": "/portfolio/static/media/AO.159fb93a.png"
+  }
+]);
