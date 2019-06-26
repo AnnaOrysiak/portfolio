0a11644d99adb5da3ee21cea7f32cf4c1ed6fb06
AnnaOrysiak <walachan88@gmail.com>
2019-06-26 10:47:08 +0200

    fixed counter in Hobby.js

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -3,52 +3,50 @@ import { InView } from 'react-intersection-observer';
 
 const Hobby = (props) => {
 
-  const { desc, img, alt, value } = props.properties;
+  const { id, desc, img, alt, value } = props.properties;
 
-  let counter = (counterOn = false) => {
+  let counter = (active, e) => {
 
-    if (counterOn) {
-      const hobbyNumbers = document.getElementsByClassName('number');
+    if (active) {
 
-      const checkCounter = () => {
-        if (counterOn) {
-          for (const hobbyNumber of hobbyNumbers) {
-            const hobbyValue = hobbyNumber.value;
-            let currentValue = 0;
-            let intervalTime = 200;
+      const currentHobby = document.getElementById(e);
+      const hobbyValue = currentHobby.value;
+      let currentValue = 0;
+      let intervalTime = 200;
 
-            const count = () => {
-              if (currentValue < hobbyNumber.value) currentValue++;
-              hobbyNumber.textContent = currentValue;
+      if (hobbyValue > 100) intervalTime = 20;
+      else if (hobbyValue < 100 && hobbyValue > 10) intervalTime = 30;
+      else intervalTime = 200;
+
+      let count = () => {
+
+        const checkCounter = () => {
+
+          if (currentValue < hobbyValue) currentValue++;
+          currentHobby.textContent = currentValue;
+
+          if (currentValue < hobbyValue) {
+            if (currentValue <= 1) {
+              count = setInterval(count, intervalTime)
             }
-
-            if (hobbyValue > 100) intervalTime = 20;
-            else if (hobbyValue < 100 && hobbyValue > 10) intervalTime = 30;
-            else intervalTime = 200;
-
-            if (currentValue < hobbyValue) {
-              if (currentValue === 0) {
-                counter = setInterval(count, intervalTime)
-              }
-            } else { clearInterval(counter) }
-
-          }
+          } else { clearInterval(count) }
         }
+        checkCounter();
 
-        counterOn = false;
       }
-      checkCounter();
+      count();
+      active = false;
     }
   }
 
   return (
-    <InView as="div" className="stats" onChange={counter} triggerOnce={true} threshold={.9}>
+    <div className="stats">
       <div className="statImg">
         <img src={img} alt={alt} />
       </div>
-      <button className="number" value={value} disabled>0</button>
+      <InView as="button" className="number" id={id} onChange={(inView) => counter(inView, id)} triggerOnce={true} threshold={.9} value={value} disabled>0</InView>
       <p className="hobby">{desc}</p>
-    </InView>
+    </div>
   )
 }
 
