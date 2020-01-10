987923ed5244c2048501707db86f8261eb449d25
AnnaOrysiak <walachan88@gmail.com>
2020-01-10 13:54:33 +0100

    Updates

--- precache-manifest.40e5c5cea341dbed99fea0f9d25b2f58.js
+++ precache-manifest.40e5c5cea341dbed99fea0f9d25b2f58.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "214883f23f40d9a566d8f4bce993dfd8",
+    "revision": "54fe033c5b170c34b61a32cfab560ec3",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "971e46f578beef2a57be",
-    "url": "/portfolio/static/css/main.821d4b5f.chunk.css"
+    "revision": "ad7e907d1d37a1c4ed15",
+    "url": "/portfolio/static/css/main.49dbf8b5.chunk.css"
   },
   {
-    "revision": "2062e4284d7dd13cceb0",
-    "url": "/portfolio/static/js/2.883ab809.chunk.js"
+    "revision": "c0efe0a329491f7e370d",
+    "url": "/portfolio/static/js/2.8629305c.chunk.js"
   },
   {
-    "revision": "971e46f578beef2a57be",
-    "url": "/portfolio/static/js/main.fdb5f255.chunk.js"
+    "revision": "ad7e907d1d37a1c4ed15",
+    "url": "/portfolio/static/js/main.880907f6.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.441d422360dfa30886f2555122b194b1.js"
+  "/portfolio/precache-manifest.40e5c5cea341dbed99fea0f9d25b2f58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
